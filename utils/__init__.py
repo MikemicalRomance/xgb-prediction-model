2b6c6386db56f4b375249635091bb0a8eccc2a07
Michael  Smith <MichaelSmith@pc-74-98.customer.ask4.lan>
2022-03-29 12:14:54 +0100

    feat: tidy-up black formatting

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,9 @@
 from .data import get_dataset
-from .features import binary_encode, binary_decode,label_encode
-from .models import train_test_valid_split, train_xgb_classifier, model_performance, predict_function
+from .features import binary_encode, binary_decode, label_encode
+from .models import (
+    train_test_valid_split,
+    train_xgb_classifier,
+    model_performance,
+    predict_function,
+)
 from .visualization import eval_metric_plot
